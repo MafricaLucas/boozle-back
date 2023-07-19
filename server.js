77f3da6b2756476f289adcc29fc7fd9c6573407f
mafricalucas <lucasmafrica57@gmail.com>
2023-07-19 10:48:25 +0200

    healthcheck

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const mariadb = require('mariadb');
+
+require('dotenv').config();
+
+const pool = mariadb.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    connectionLimit: 5
+});
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.get('/healthcheck', async (req, res) => {
+    let conn;
+    try {
+        conn = await pool.getConnection();
+        const status = conn.isValid() ? 'alive' : 'dead';
+        res.json({
+            status: 'success',
+            data: {
+                dbStatus: status
+            }
+        });
+    } catch (err) {
+        console.log(err);
+        res.json({
+            status: 'fail',
+            data: {
+                dbStatus: 'dead'
+            }
+        });
+    } finally {
+        if (conn) conn.end();
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
