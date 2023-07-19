a357265c4d08ee316601d003d5572048f61bd7f1
mafricalucas <lucasmafrica57@gmail.com>
2023-07-19 15:41:22 +0200

    make sure not create 2 same conv

--- api/conversationsRoutes.js
+++ api/conversationsRoutes.js
@@ -9,10 +9,24 @@ router.post('/', async (req, res) => {
         conn = await pool.getConnection();
         const { user1Id, user2Id } = req.body;
 
+        // Check if a conversation already exists between the two users
+        const [existingConvo] = await conn.query(
+            'SELECT * FROM Conversations WHERE (User1Id = ? AND User2Id = ?) OR (User1Id = ? AND User2Id = ?)',
+            [user1Id, user2Id, user2Id, user1Id]
+        );
+
+        if (existingConvo.length > 0) {
+            return res
+                .status(409)
+                .json({ message: 'Conversation already exists.' });
+        }
+
+        // If no existing conversation, create a new one
         await conn.query(
             'INSERT INTO Conversations (User1Id, User2Id) VALUES (?, ?)',
             [user1Id, user2Id]
         );
+
         res.status(201).json({ message: 'Conversation created successfully.' });
     } catch (err) {
         console.log(err);
