20145e17f8795e4568f61b80c5b7cd1054a6cf51
mafricalucas <lucasmafrica57@gmail.com>
2023-07-21 09:32:20 +0200

    fix url

--- server.js
+++ server.js
@@ -4,15 +4,16 @@ const routes = require('./routes');
 const app = express();
 const port = process.env.PORT || 82;
 
-app.use(express.urlencoded({ limit: '10mb', extended: true }));
+// Utiliser express.json avant express.urlencoded
+app.use(express.json({ limit: '10mb' }));
 
 app.use('/images', express.static('/images'));
 
-// Appliquer les routes AVANT express.json
+// Appliquer les routes APRES express.json
 app.use('/', routes);
 
-// Utiliser express.json après les routes
-app.use(express.json({ limit: '10mb' }));
+// Utiliser express.urlencoded après les routes
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
