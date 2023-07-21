38e1610546a7e3f2e31625ddbff0ea47d309f59d
mafricalucas <lucasmafrica57@gmail.com>
2023-07-21 07:29:09 +0200

    fix order

--- server.js
+++ server.js
@@ -4,12 +4,15 @@ const routes = require('./routes');
 const app = express();
 const port = process.env.PORT || 82;
 
-app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 app.use('/images', express.static('/images'));
 
+// Appliquer les routes AVANT express.json
 app.use('/', routes);
+
+// Utiliser express.json après les routes
+app.use(express.json({ limit: '10mb' }));
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
