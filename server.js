2a469f24ee90a98447afaf22c7ac2f3573eba538
mafricalucas <lucasmafrica57@gmail.com>
2023-07-21 07:24:57 +0200

    fix limit

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const routes = require('./routes');
 const app = express();
 const port = process.env.PORT || 82;
 
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 app.use('/images', express.static('/images'));
 
