1e77793aca3878972d0fcc13c2a5ecafa956dec3
mafricalucas <lucasmafrica57@gmail.com>
2023-07-21 09:30:16 +0200

    export images routes

--- api/userRoutes.js
+++ api/userRoutes.js
@@ -4,33 +4,6 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const { validateUser, validateLogin } = require('../validators');
 const { validationResult } = require('express-validator');
-const multer = require('multer');
-const fs = require('fs');
-const path = require('path');
-const sharp = require('sharp');
-const authenticate = require('../authenticate');
-
-function multerErrorHandling(err, req, res, next) {
-    if (err instanceof multer.MulterError) {
-        res.status(500).send({ message: `Multer error: ${err.message}` });
-    } else if (err) {
-        res.status(500).send({ message: `General error: ${err.message}` });
-    }
-    next(err);
-}
-
-const upload = multer({
-    dest: '/images',
-    limits: {
-        fileSize: 5 * 1024 * 1024 // 5MB
-    },
-    fileFilter: (req, file, cb) => {
-        if (!file.mimetype.startsWith('image/')) {
-            return cb(null, false, new Error('Not an image'));
-        }
-        cb(null, true);
-    }
-});
 
 require('dotenv').config();
 
@@ -148,68 +121,5 @@ router.get('/search/:pseudo', async (req, res) => {
         if (conn) conn.end();
     }
 });
-
-router.post(
-    '/profileImage',
-    authenticate,
-    upload.single('image'),
-    multerErrorHandling,
-    async (req, res) => {
-        let conn;
-        try {
-            conn = await pool.getConnection();
-            const { file } = req;
-
-            if (!file) {
-                return res.status(400).json({ message: 'No file provided.' });
-            }
-
-            // resize image and move to correct directory
-            const newFilename = `/app/images/${file.filename}`;
-            await sharp(file.path)
-                .resize(720, 720, {
-                    fit: 'inside', // maintain aspect ratio
-                    withoutEnlargement: true // avoid enlarging smaller images
-                })
-                .toFile(newFilename); // path where resized image is saved
-
-            // Delete original uploaded image from tmp directory
-            fs.unlink(path.join('/app/images', file.path), (err) => {
-                if (err) console.log(err);
-            });
-
-            // Get the old image url.
-            const [user] = await conn.query(
-                'SELECT ProfileImageUrl FROM Users WHERE Id = ?',
-                [req.user.id]
-            );
-            const oldImageUrl = user ? user.ProfileImageUrl : null;
-
-            // Update the user's profile image URL in the database.
-            await conn.query(
-                'UPDATE Users SET ProfileImageUrl = ? WHERE Id = ?',
-                [newFilename, req.user.id]
-            );
-
-            // If an old image exists, delete it.
-            if (oldImageUrl) {
-                const oldImagePath = path.join(__dirname, '../', oldImageUrl);
-                fs.unlink(oldImagePath, (err) => {
-                    if (err) console.log(err);
-                });
-            }
-
-            res.json({ message: 'Image uploaded successfully.' });
-        } catch (err) {
-            console.log(err);
-            res.status(500).json({
-                message:
-                    'An error occurred while processing your request. Please try again later.'
-            });
-        } finally {
-            if (conn) conn.end();
-        }
-    }
-);
 
 module.exports = router;
--- routes.js
+++ routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const healthcheckRoutes = require('./api/healthcheckRoutes');
 const userRoutes = require('./api/userRoutes');
+const imagesRoutes = require('./api/imageRoutes');
 const messagesRoutes = require('./api/messagesRoutes');
 const conversationsRoutes = require('./api/conversationsRoutes');
 
@@ -8,6 +9,7 @@ const router = express.Router();
 
 router.use('/healthcheck', healthcheckRoutes);
 router.use('/users', userRoutes);
+router.use('/images', imagesRoutes);
 router.use('/messages', messagesRoutes);
 router.use('/conversations', conversationsRoutes);
 
